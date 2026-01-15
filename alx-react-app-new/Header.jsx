121344dedea555c018daba86454f1f800c046d80
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-15 23:50:14 +0100

    Refactor Header component with padding style

--- alx-react-app-new/Header.jsx
+++ alx-react-app-new/Header.jsx
@@ -1,3 +1,16 @@
-<header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-   <h1>My Favorite Cities</h1>
- </header>
+function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        textAlign: "center",
+        padding: "20px"
+      }}
+    >
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+export default Header;
