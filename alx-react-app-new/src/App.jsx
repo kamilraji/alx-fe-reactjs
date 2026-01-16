1d7795758f07d021e516be3d1dfeea4a2a849bf9
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-16 07:52:15 +0100

    Add UserContext and ProfilePage to App component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -32,3 +32,24 @@ function App() {
 }
 
 export default App;
+
+
+
+
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
+
+function App() {
+  const userData = {
+    name: "Jane Doe",
+    email: "jane.doe@example.com",
+  };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
