282f42b9bb70909c3d6532b8dcef4d0e99d35932
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-16 07:43:22 +0100

    Integrate Counter component into App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -19,3 +19,16 @@ function App() {
 }
 
 export default App;
+
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
