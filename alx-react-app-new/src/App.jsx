91f0944dec62267292d57e3df3ee6d3ee8dcaa45
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-16 08:02:08 +0100

    Update App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,37 +1,3 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import UserProfile from "./components/UserProfile";
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <UserProfile
-        name="John Doe"
-        age={25}
-        bio="Frontend developer passionate about React."
-      />
-      <Footer />
-    </>
-  );
-}
-
-export default App;
-
-import Counter from "./components/Counter";
-
-function App() {
-  return (
-    <div>
-      <h1>Simple Counter App</h1>
-      <Counter />
-    </div>
-  );
-}
-
-export default App;
 
 
 
