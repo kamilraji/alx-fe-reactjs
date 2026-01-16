1b932ed70646b741b09a079bd394f61cf67eeb4b
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-16 08:52:11 +0100

    Pass user data to UserInfo component

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
+import UserContext from "./context/UserContext";
 import UserInfo from "./UserInfo";
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+  return <UserInfo userData={userData} />;
 }
 
 export default UserProfile;
