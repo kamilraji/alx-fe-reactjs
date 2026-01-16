413577f7827f9dde4639f49f48d42f4196b32c75
kamil <124085890+kamilraji@users.noreply.github.com>
2026-01-16 09:04:30 +0100

    Update user email from example.com to gmail.com

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,7 +4,7 @@ import UserContext from "./UserContext";
 function App() {
   const userData = {
     name: "Jane Doe",
-    email: "jane.doe@example.com",
+    email: "jane.doe@gmail.com.com",
   };
 
   return (
