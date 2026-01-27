27ba1a2f829af527324f98bd176c73f201137cf2
Macbook Pro 2020 <admin@MacBook-Pro.local>
2026-01-27 17:08:30 +0100

    Task 2: Wire Search component to GitHub API

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import Search from "./components/Search"
+import { useState } from "react";
+import Search from "./components/Search";
+import { searchUsers } from "./services/githubService";
 
-const App = () => {
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = async (searchTerm) => {
+    const results = await searchUsers(searchTerm);
+    setUsers(results);
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>GitHub User Search</h1>
-      <Search />
+      <Search onSearch={handleSearch} />
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
