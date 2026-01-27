cf6f36c710834e1e6b6628cf3318bef55bdfb92f
Macbook Pro 2020 <admin@MacBook-Pro.local>
2026-01-27 17:46:12 +0100

    Task 2: Add fetchUserData GitHub API service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from "axios"
 
-const GITHUB_API_URL = "https://api.github.com/users";
+const BASE_URL = "https://api.github.com/users"
 
-export const fetchUser = async (username) => {
-  const response = await axios.get(`${GITHUB_API_URL}/${username}`);
-  return response.data;
-};
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`${BASE_URL}/${username}`)
+  return response.data
+}
 
