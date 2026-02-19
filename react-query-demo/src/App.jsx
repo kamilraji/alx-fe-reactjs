3923170fe1b6c567d50418598c7b44ffe29bd2a8
Macbook Pro 2020 <admin@MacBook-Pro.local>
2026-02-19 21:27:04 +0100

    Add react-query-demo project with React Query implementation

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import PostsComponent from "./components/PostsComponent";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
+
